db63f4fae04878e611f5879436ba557b64e4ca9e
Clyde <clyderadioo@gmail.com>
2023-07-21 15:18:51 +0200

    update nom
    dans le addOrUpdateTopic je change le name="nom", par name="title"

--- view/forum/addOrUpdateTopic.php
+++ view/forum/addOrUpdateTopic.php
@@ -1,7 +1,7 @@
 <form action="index.php?ctrl=forum&action=addTopic" method="post"> <!--  le format qui permetre de ajouter un categorie   -->
 
     <label for="nomGenre">Nom de Topic</label>
-    <input type="text" id="nom" name="nom"  />
+    <input type="text" id="nom" name="title"  />
     <!-- id pour recevoir le focus sur l'input quand on clique sur le label correspondant (label.for == input.id) -->
     <!-- name pour que la value soit envoyée dans le formulaire en POST (sera récupéré dans le contrôleur) -->
 
